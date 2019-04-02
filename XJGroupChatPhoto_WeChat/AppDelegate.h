609e704cde498ecc3c6ad6b19db9841effc7caaa
xiongjiwei <877703715@qq.com>
2019-04-02 12:18:50 +0800

    创建工程

---
+++ XJGroupChatPhoto_WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJGroupChatPhoto_WeChat
+//
+//  Created by mac on 2019/4/2.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJGroupChatPhoto_WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJGroupChatPhoto_WeChat
+//
+//  Created by mac on 2019/4/2.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
